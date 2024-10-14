d66ed93e8a91a969eac236e01da7f85423789611
Tiemo Lamers <t.j.lamers@students.uu.nl>
2024-10-14 22:03:11 +0200

    Interaction with user made in main class

--- MSO opdracht 2/Main.cs
+++ MSO opdracht 2/Main.cs
@@ -8,28 +8,60 @@ namespace MSO_opdracht_2
 	{
 		public static void Main(string[] args)
 		{
-			Program program = new Program();
+			Program program= new Program();
 			Translator translator = new Translator();
 			Calculator calculator = new Calculator();
-			Move move = new Move(5);
-			program.AddTask(new Move(5));
-			program.AddTask(new Turn("right"));
-			program.AddTask(new Move(3));
 
-			List<ITask> repeatTasks = new List<ITask>()
+
+			Console.WriteLine("Do you want to use one of the example programs or import one? Type 'Example' or 'Import'");
+			string choice = Console.ReadLine();
+			while (choice != null) 
 			{
-				new Turn("left"),   // Turn left (from South to East)
-                new Move(2)         // Move 2 units East
-            };
-			program.AddTask(new Repeat(2, repeatTasks));  // Repeat those 2 tasks twice
-
-			string inputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Input1.txt");
-			inputFilePath = Path.GetFullPath(inputFilePath);  // Resolves the relative path to an absolute path
-
-			Program program1 = translator.Translate(inputFilePath);
-			program1.Run();
-			Console.WriteLine();
-			Console.WriteLine($"commands: {calculator.numOfCommands(program1)}, repeats: {calculator.numOfRepeat(program1)}");
+				if (choice == "Example")
+				{
+					Console.WriteLine("What example? Type 'Basic', 'Advanced', 'Expert'");
+					string exampleType = Console.ReadLine();
+					while (exampleType != null)
+					{
+						if (exampleType == "Basic")
+						{
+							//program = basicProgram;
+							//break;
+						}
+						else if (exampleType == "Advanced")
+						{
+							//program = advancedProgram;
+							//break;
+						}
+						else if (exampleType == "Expert")
+						{
+							//program = expertProgram;
+							//break;
+						}
+						else
+						{
+							Console.WriteLine("Wrong answer. Type 'Basic', 'Advanced', 'Expert'");
+							exampleType = Console.ReadLine();
+						}
+					}
+					break;
+				}
+				else if (choice == "Import")
+				{
+					Console.WriteLine("Insert the text file into the same file as the cs files and type the filename here");
+					string filename = Console.ReadLine();
+					string inputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\", filename);
+					inputFilePath = Path.GetFullPath(inputFilePath);
+					program = translator.Translate(inputFilePath);
+					break;
+				}
+				else
+				{
+					Console.WriteLine("Wrong answer. Type 'Example' or 'Import'");
+					choice = Console.ReadLine();
+				}
+			}
+			program.Run();
 		}
 	}
 }
